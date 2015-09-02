de195d57243a0a67c8bbd4cad3391069f4336521
Blu <Blu@TXHOUS10PC691.wm.com>
2015-09-01 23:48:48 -0500

    Added spring rest and json

---
+++ myontimenotice/src/com/mkyong/model/Customer.java
@@ -0,0 +1,71 @@
+package com.mkyong.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
+public class Customer implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4276019548639284632L;
+
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	
+	@Persistent
+    private String name;
+	
+	@Persistent
+    private String email;
+	
+	@Persistent
+    private Date date;
+
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Customer() {
+		super();
+	}
+
+	
+}
