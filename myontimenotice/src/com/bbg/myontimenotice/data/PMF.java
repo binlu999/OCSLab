9cf476594fe73db88c236be059f70c09275b5dc3
Blu <Blu@TXHOUS10PC691.wm.com>
2015-09-07 19:05:00 -0500

    ADDED EMAIL

--- myontimenotice/src/com/bbg/myontimenotice/data/PMF.java
+++ myontimenotice/src/com/bbg/myontimenotice/data/PMF.java
@@ -1,4 +1,4 @@
-package com.mkyong;
+package com.bbg.myontimenotice.data;
 
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
--- myontimenotice/src/com/mkyong/controller/CustomerController.java
+++ myontimenotice/src/com/mkyong/controller/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.mkyong.PMF;
+import com.bbg.myontimenotice.data.PMF;
 import com.mkyong.model.Customer;
 
 @Controller
